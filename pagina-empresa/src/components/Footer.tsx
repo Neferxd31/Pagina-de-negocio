0d22a42bf538784b48b6ce1870b6ca279e1609d8
Sebs_music_live <lopezortegajohansebastian@gmail.com>
2026-01-13 17:44:15 -0500

    Se agrega proyecto con react, tailwind CSS, Typescript y Docker

---
+++ pagina-empresa/src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+    <footer className="py-10 border-t border-darkBorder text-center text-muted px-6">
+        © 2024 KorexalTech. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    </footer>
+    )
+}
